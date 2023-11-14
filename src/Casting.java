2eef1060b9f083c9f60316840f98aa1c32c7620e
jhon <pabongamas@gmail.com>
2023-11-14 17:59:32 -0500

    actualizacion

---
+++ src/Casting.java
@@ -0,0 +1,13 @@
+public class Casting {
+    public static void main(String[] args) {
+        //en un año ubicar 30 perritos
+        //cuantos perritos ubique al mes
+
+        double monthlyDogs=30.0/12.0;
+        System.out.println(monthlyDogs);
+
+        //ESTIMACION
+        int estimatedMonthlyDogs=(int) monthlyDogs;
+        System.out.println(estimatedMonthlyDogs);
+    }
+}
--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,5 +1,45 @@
 public class DataTypes {
     public static void main(String[] args) {
-        int n=
+        //byte, short, int, long
+        System.out.println("bits tipo byte:" + Byte.SIZE);
+        System.out.println("bytes tipo byte:" + Byte.BYTES);
+        System.out.println("valor minimo tipo byte:" + Byte.MIN_VALUE);
+        System.out.println("valor maximo tipo byte:" + Byte.MAX_VALUE);
+        System.out.println();
+        System.out.println("bits tipo short:" + Short.SIZE);
+        System.out.println("bytes tipo short:" + Short.BYTES);
+        System.out.println("valor minimo tipo short:" + Short.MIN_VALUE);
+        System.out.println("valor maximo tipo short:" + Short.MAX_VALUE);
+        System.out.println();
+        System.out.println("bits tipo int:" + Integer.SIZE);
+        System.out.println("bytes tipo int:" + Integer.BYTES);
+        System.out.println("valor minimo tipo int:" + Integer.MIN_VALUE);
+        System.out.println("valor maximo tipo int:" + Integer.MAX_VALUE);
+        System.out.println();
+        System.out.println("bits tipo long:" + Long.SIZE);
+        System.out.println("bytes tipo long:" + Long.BYTES);
+        System.out.println("valor minimo tipo long:" + Long.MIN_VALUE);
+        System.out.println("valor maximo tipo long:" + Long.MAX_VALUE);
+
+        byte byteVar = 15;
+        System.out.println("byteVar = " + byteVar);
+        short shortVar = 100;
+        System.out.println("shortVar = " + shortVar);
+        int intVar = 500;
+        System.out.println("intVar = " + intVar);
+        long longVar = 1000;
+        System.out.println("longVar = " + longVar);
+
+        var numero = 10;
+        System.out.println("numero = " + numero);
+        var salary=1000;
+        var pension=salary*0.03;
+        var totalSalary=salary-pension;
+        System.out.println(salary);
+        System.out.println(pension);
+        System.out.println(totalSalary);
+
+        var employeeName="Anahi salgado";
+        System.out.println("employee:"+employeeName+" salary: "+totalSalary);
     }
 }
